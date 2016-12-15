cf64d0faf92d0cf102223ec14aa01347d6d85552
Luis <webdeveloperpr@gmail.com>
2016-12-15 00:54:04 -0400

    Updated postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const smartImport = require("postcss-smart-import");
 const autoprefixer = require("autoprefixer");
 const browserlist = require('./browserlist');
 
-module.exports = function (webpack) {
+module.exports = function(webpack) {
   return [
     smartImport({ addDependencyTo: webpack }),
     autoprefixer({ browsers: browserlist }),
